c7ba635bec71dcf19e689e9c6e216925b3a7a73b
Priit Lätt <priit.latt@lab.mobi>
2014-02-19 11:07:14 +0200

    Add some mock tests

---
+++ GreenhouseCI/src/instrumentTest/java/com/greenhouseci/test/MixedTest.java
@@ -0,0 +1,46 @@
+package com.greenhouseci.test;
+
+import android.test.AndroidTestCase;
+
+import java.lang.Exception;
+
+public class MixedTest extends AndroidTestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testSuccess1() throws Exception {
+        assertEquals(1, 1);
+        assertNotSame(1, 2);
+        assertTrue(true);
+        assertFalse(false);
+        assertTrue(true || false);
+        assertFalse(true && false);
+    }
+
+    public void testFail1() throws Exception {
+        assertFalse(true);
+        assertTrue(false);
+    }
+
+    public void testSuccess2() throws Exception {
+        assertEquals(1, 1);
+        assertNotSame(1, 2);
+        assertTrue(true);
+        assertFalse(false);
+        assertTrue(true || false);
+        assertFalse(true && false);
+    }
+
+    public void testFail2() throws Exception {
+        assertFalse(true);
+        assertTrue(false);
+    }
+}
